91b4b48a2d3c3d65b07397f95244c90ef59f7e15
Alexandr <ussov39@gmail.com>
2022-09-18 11:44:04 +0300

    created login, logout and user profile

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import index, other_page
+from .views import index, other_page, BBLoginView, profile, BBLogoutView
 
 app_name = 'main'
 urlpatterns = [
     path('<str:page>/', other_page, name='other'),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout')
     ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
+
 def index(request):
     return render(request, 'main/index.html')
 
@@ -13,3 +22,7 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
