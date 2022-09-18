030b824bee9e2ba294da97d7489e3f2edd20c114
Alexandr <ussov39@gmail.com>
2022-09-18 18:31:03 +0300

    added change password page

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView
 
 app_name = 'main'
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
-    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change')
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change')
     ]
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
@@ -36,6 +36,11 @@ class BBLoginView(LoginView):
 
 class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'main/logout.html'
+
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    send_message = 'Пользователь успешно изменен'
 
 def index(request):
     return render(request, 'main/index.html')
