18938e74089da9e0b3dabd5a57d3b1207c8e879b
Alexandr <ussov39@gmail.com>
2022-09-24 22:38:11 +0300

    added rubric operations and adds operations

--- bboard/settings.py
+++ bboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'django_cleanup',
+    'easy_thumbnails',
     'main'
 ]
 
@@ -129,3 +132,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'main.AdvUser'
 
 EMAIL_PORT = 1025
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+THUMBNAIL_ALIASES = {
+        '': {
+            'default': {
+                'size': (96, 96),
+                'crop': 'scale'
+                }
+            }
+    }
+
+THUMBNAIL_BASEDIR = 'thumbnails'
