3c773c57d7b4d640805e5317edf0c504fae6bb74
Alexandr <ussov39@gmail.com>
2022-09-25 12:42:55 +0300

    added api

--- bboard/settings.py
+++ bboard/settings.py
@@ -43,17 +43,22 @@ INSTALLED_APPS = [
     'django_cleanup',
     'easy_thumbnails',
     'captcha',
-    'main'
+    'main',
+    'rest_framework',
+    'corsheaders',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  
 ]
 
 ROOT_URLCONF = 'bboard.urls'
@@ -148,3 +153,7 @@ THUMBNAIL_ALIASES = {
     }
 
 THUMBNAIL_BASEDIR = 'thumbnails'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_URLS_REGEX = r'^/api/.*$'
